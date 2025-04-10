4dfadb2ad1153d7afdd10f5cbc123eb9fcd98633
Ritesh <riteshhattarki@icloud.com>
2025-04-10 14:44:59 -0400

    modified calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+# GitHub Link: https://github.com/Revengestyle6/lab10-LP-RH.git
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +8,30 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b): # Performing b / a
+    if a == 0:
+        raise ZeroDivisionError
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError
+    else:
+        result = math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
+
+
+
+
 
 
